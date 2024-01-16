e537643acdf4f70b35fcba28cdc2e53eb92e4167
D0N4T024 <103001490+D0N4T024@users.noreply.github.com>
2024-01-16 13:29:33 -0400

    Removal of unnecessary content
    Elimination of headings and unnecessary texts.
    
    Elimination of unnecessary imports.
    
    Stop using client render.

--- Client/come-ya/src/app/page.js
+++ Client/come-ya/src/app/page.js
@@ -1,8 +1,4 @@
-"use client"
-import Image from 'next/image'
 import styles from './page.module.css'
-import Link from 'next/link'
-import { Heading, Text } from '@chakra-ui/react'
 import Carousel from '../components/Carousel/Carousel'
 import MainCarousel from '../components/MainCarousel/MainCarousel'
 
@@ -18,60 +14,35 @@ export default function Home() {
           <div className={styles.category}>
               <div className={styles.categoryBottomSide}>
                 <Carousel
+                  titulo={'Restaurantes'}
+                />
+              </div>
+          </div>
+
+          <div className={styles.category}>
+              <div className={styles.categoryBottomSide}>
+                <Carousel
                   titulo={'Ofertas de hoy'}
-                  //Proximanente: props para filtrar fotos
-                  />
+                />
               </div>
           </div>
 
           <div className={styles.category}>
               <div className={styles.categoryBottomSide}>
                 <Carousel
-                  titulo={'Restaurantes'}/>
+                  titulo={'Combos especiales'}
+                />
               </div>
           </div>
 
           <div className={styles.category}>
               <div className={styles.categoryBottomSide}>
                 <Carousel
-                  titulo={'Combos especiales'}/>
+                  titulo={'Bebidas'}
+                />
               </div>
           </div>
-
-          <div className={styles.category}>
-              <div className={styles.categoryBottomSide}>
-                <Carousel
-                  titulo={'Bebidas'}/>
-              </div>
-          </div>
-
-          
-
-          <div>
-            <Heading as='h3' size='lg'>Restaurantes</Heading>
-          </div>
-
-          <div>
-            <Heading as='h3' size='lg'>Ofertas de hoy</Heading>
-          </div>
-
-          <div>
-            <Heading as='h3' size='lg'>Bebidas&</Heading>
-          </div>
-
-          <div>
-            <Heading as='h3' size='lg'>Ofertas de hoy</Heading>
-          </div>
-
-
-          <div>
-            <Link href="/Screens/Post">
-              Poner espera&
-            </Link>
-          </div>
-          
         </div>
-        
       </div>
     </main>
   )
